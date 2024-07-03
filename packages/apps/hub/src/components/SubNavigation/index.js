b426d43bf369a15ae40022394166718487fe2e3d
g.j.hilton <guyjohnhilton@gmail.com>
2024-07-03 09:45:38 +0100

    grey

--- packages/apps/hub/src/components/SubNavigation/index.js
+++ packages/apps/hub/src/components/SubNavigation/index.js
@@ -5,13 +5,14 @@ import { childrenOfNamedSiteItem } from '@/util/content'
 import {NavigationItem} from "@/components/NavigationItem"
 
 export const SubNavigation = ({ selected }) => (
-	<PageMargin>
-	<nav className={styles.nav}>
+
+	<nav className={styles.nav}>	<PageMargin>
 		<ol>
 			{childrenOfNamedSiteItem("developers").map(item => (
 				<NavigationItem key={item.urlPath} styles={styles} selected={selected === item.name} {...item} />
 			))}
 		</ol>
+		</PageMargin>
 	</nav>
-	</PageMargin>
+	
 )
