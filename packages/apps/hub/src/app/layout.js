8814b1ef7584ce9cd18305552ca7720945bb3e1f
g.j.hilton <guyjohnhilton@gmail.com>
2024-07-13 03:41:04 +0100

    Revert "Merge branch 'content' into development"
    This reverts commit 65fd14baf177995f2dfdab87d97dd1ad7be2b0e2, reversing
    changes made to ac58005d96526e3aaafd41795d74a73d97d88f71.

---
+++ packages/apps/hub/src/app/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google'
+import { Footer } from '@/components/Footer/index'
+//import _ from '@swirrl/CssReset' // eslint-disable-line no-unused-vars
+import './tokens.css'
+import './mvp.css' // temporary placeholder styles
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+	title: 'Open Referral UK',
+	description: 'Description here'
+}
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				{children}
+				<Footer />
+			</body>
+		</html>
+	)
+}
